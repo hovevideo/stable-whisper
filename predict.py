451533c93fa181e4321a72e31889cb71a3c7ec76
Luiz <cieslakluiz@gmail.com>
2024-05-09 15:17:12 -0300

    feat: choose output + cleanup_json_fields

--- predict.py
+++ predict.py
@@ -6,9 +6,31 @@ import stable_whisper
 import requests
 import mimetypes
 
-# import json
+import json
 
 filename = "input"
+
+
+def cleanup_json_fields(file):
+    root_fields_to_delete = ["regroup_history", "nonspeech_sections", "ori_dict"]
+    segment_fields_to_keep = ["start", "end", "text"]
+    with open(file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        print(data["text"])
+        for segment in list(data):
+            if segment in root_fields_to_delete:
+                del data[segment]
+
+        for segment in data["segments"]:
+            # trim segment string
+            segment["text"] = segment["text"].strip()
+
+            for field in list(segment):
+                if field not in segment_fields_to_keep:
+                    del segment[field]
+
+    with open(file, "w") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
 
 
 class Predictor(BasePredictor):
@@ -18,10 +40,12 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
-        url: str = Input(description="File URL"),
-        # scale: float = Input(
-        #     description="Factor to scale image by", ge=0, le=10, default=1.5
-        # ),
+        url: str = Input(description="Audio or video URL"),
+        output_format: str = Input(
+            choices=["ass", "json"],
+            default="json",
+            description="Output format: ass (ASS subtitles) or json (transcription in JSON format).",
+        ),
     ) -> Path:
         """Download and save the file to disk"""
         response = requests.get(url)
@@ -36,10 +60,10 @@ class Predictor(BasePredictor):
         # TODO arg
         result.split_by_length(max_words=4)
 
-        # TODO arg, choose output?
-        result.to_ass(f"{filename}.ass")
-        result.save_as_json(f"{filename}.json")
-        return Path(f"{filename}.json")
-        # with open(f"{filename}.json") as f:
-        #     d = json.load(f)
-        #     return d
+        if output_format == "ass":
+            result.to_ass(f"{filename}.ass")
+            return Path(f"{filename}.ass")
+        elif output_format == "json":
+            result.save_as_json(f"{filename}.json")
+            cleanup_json_fields(f"{filename}.json")
+            return Path(f"{filename}.json")
